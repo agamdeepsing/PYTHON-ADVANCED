1337cfb0ca10df091f04f6b6b5bb3811f5616df3
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-06 22:29:34 +0530

    Add files via upload

---
+++ PYTHON ADVANCED 12.py
@@ -0,0 +1,41 @@
+
+# Ques 1
+ String’s indexed character cannot to be assigned a New value , as Strings are immutable.
+
+# Ques 2 
++= operator is used to concatenate strings, it does not violate Python’s string immutability Property.
+
+# Ques 3
+A Character in string can be indexed using string name followed by the index number of character in square bracket. Positive Indexing or Negative Indexing.
+
+# Ques 4
+We can access elements of sequence datatypes by using slicing and indexing. Indexing is used to obtain a individual element while slicing used for sequence of elements
+
+# Ques 5
+Indexed characters and sliced substrings have datatype String.
+
+# Ques 6
+In Python, a string is a sequence of characters, and each character within a string is treated as a single element.
+for eg :
+str = "how are you"
+character = str[0]
+
+# Ques 7
++, += and * allow to combine one or more smaller strings to create a larger string. <string>.join(<sep>) method joins element of iterable type i.e list and tuple to get a combined string.
+
+# Ques 8
+Checking the target string with in or not Operators before using the index method to find a substring just helps confirming availability of substring and thus avoid raising of ValueError.
+
+# QUes 9
+The String Operators and built-in methods to Produce Simple Boolean (True/False) Results are:
+
+in
+not
+<string>.isalpha()
+<string>.isalnum()
+<string>.isdecimal()
+<string>.isdigit()
+<string>.islower()
+<string>.isnumeric()
+<string>.istitle() and many more
+
