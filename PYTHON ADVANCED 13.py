1e8f2fe3a5a99865b9a19343398deac793577ac9
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-07 23:14:50 +0530

    Add files via upload

---
+++ PYTHON ADVANCED 13.py
@@ -0,0 +1,33 @@
+
+# Ques 1 
+listt = [1,2,3,4,5,6,7,8,9,4,5,6,5,2] 
+def index(ineuorn_list,position):
+    return ineuorn_list[position]
+print(index(listt[7]))
+print(index(listt[-3]))
+
+
+# Ques 2
+my_list = [0 for i in range(1001)]
+print(my_list)
+
+
+# Ques 3
+my_list = [x for x in range(1,15)]
+sliced_list = my_list[::2]
+print(sliced_list)
+
+# Ques 4
+Indexing is used to access a specific element in the sequence,Slicing, on the other hand, is used to extract a range of elements from the sequence.
+
+# Ques 5
+If start index is out of range then it will return empty entity.
+
+# Ques 6
+Always use return statement, if we want the see the changes in the input list.
+
+# Ques 7
+In Unbalanced Matrix number of rows is not same as number of columns. 
+
+# Ques 8
+List comprehension or a Loop helps creation of large matrices easy. it also makes reading code easy.
