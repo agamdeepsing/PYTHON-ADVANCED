6fd3f77b29abb0de1941ef0f2fc0f5449684907a
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-17 22:03:21 +0530

    Add files via upload

---
+++ PYTHON ADVANCED 15.py
@@ -0,0 +1,32 @@
+
+# Ques 1
+Walrus Operator: This operator is used to assign and return a value in the same expression. This removes the need for initializing the variable upfront. The major benefit of this is it saves some lines of code.
+
+yield and return statements do not require parentheses to return multiple values.
+
+Dict comprehensions have been modified so that the key comes first and the value second.
+
+If you miss a comma in your code such as a = [(3, 9) (8, 2)], instead of throwing TypeError, it displays an informative Syntax warning.
+
+
+# Ques 2
+ In Python, the term monkey patch refers to make dynamic (or run-time) modifications to a class or module. In Python, we can actually change the behavior of code at run-time.
+
+
+# Ques 3
+deep copy- it created new and seprate copy of objects.
+any changes made does not afffect the original.
+
+shalloow copy - creates copy of the object rather than copying the elements.
+any chnages made effect the original one.
+
+# Ques 4
+n Python, the highest possible length of an identifier is 79 characters. Python is a high level programming language.
+
+
+# Ques 5
+ A generator comprehension is a single-line specification for defining a generator in Python.
+
+It is absolutely essential to learn this syntax in order to write simple and readable code.
+
+Generator comprehension uses round bracket.
