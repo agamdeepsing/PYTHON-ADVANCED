5c5f5ff0c96247afa71526d122692893c3835ff3
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-18 01:02:44 +0530

    Add files via upload

---
+++ PYTHON ADVANCED 21.py
@@ -0,0 +1,58 @@
+
+# Ques 1
+it gives us the output of how many different outcomes are in an experiment.
+
+values generated from a random process are inherently unpredictable.Based on
+the previous data and the occurences of the random event, we can predict the
+outcome in terms of probabilities.
+
+# Ques 2
+Truly random numbers are not predictable.
+All pseudo-random numbers are predictable(if can be determined).
+
+Latter is considered good enough as they have sort of uniform distribution.
+
+# Ques 3
+two main factors are mean and the variance.
+
+# Ques 4
+ A Real-life example of a normal distribution are heights or blood
+pressure measurements
+
+# Ques 5
+In the short term, a probability distribution can exhibit various 
+behaviors.
+we can expect a behaviour of probability distribution:
+1.central tendency
+2.skewness
+3.spread
+4.dispersion
+
+As the number of trials or sample size grows, the observed frequencies
+tend to converge to theoretical probabilitiesand the distribution of sample
+means tends to approach a normal distribution.
+
+# Ques 6
+list, string, and tuples can be shuffled by using random.shuffle.
+
+# Ques 7
+Conversions to Integer
+Arithmetic function
+Exponential function
+Logarithimic function
+Periodic functions
+Quadratic function
+Hyperbolic function
+Trigonometric function
+
+# Ques 8
+both exponentation and logarithm are inverse of each other.
+The standard form of exponential is ax = N
+
+logarithmic function form as logaN = x 
+
+
+# Ques 9
+log2(x) 
+log10(x)
+log1p(a)
