02e9d0d14dcbcec3869e2de27e69cfcf5cbda453
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-19 01:16:21 +0530

    Add files via upload

---
+++ PYTHON ADVANCED 16.py
@@ -0,0 +1,35 @@
+
+# Ques 1
+pattern matching, text searach , validation, maniulation, transformation, text replacrmrnt, extraction,efficiency, automation etc.
+
+# Ques 2
+Both (ab)c+ and a(bc)+ are valid patterns. the difference between both these patterns is in (ab)c+ ab is group whereas in a(bc)+ bc is a group.
+
+# ques 3
+import re statement always has to be imported before using regular expressions.
+
+# Ques 4
+ The Characters .,*,?,^,or,(), have a special signiface when used with square brackets. They need not be be explicitly escaped by \ as in case of pattern texts in a raw string.
+
+# Ques 5
+it can improved performance, better code readablity and maintainability, it can raise error if there is some syntax error.
+
+
+# Ques 6
+When using the re.match() and re.search() functions in Python's regular expression module (re), they return a match object if a match is found.
+
+examples
+accessing the match String
+extracting the matched groups
+Getting start and end positions of the match
+Splitting the string based on the matched pattern
+
+
+# Ques 7
+both have different functionalities in regular expression.
+the vertical bar (|) is used for alternation
+ square brackets ([]) define a character set
+
+
+# ques 8
+Raw Strings are used in the regular-expression search patterns, so that blackslashes do not gets escaped.
