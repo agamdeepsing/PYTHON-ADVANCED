f65cc731188c85a667f2d503883391109a4dc8fc
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-19 00:07:38 +0530

    Add files via upload

---
+++ PYTHON ADVANCED 25.py
@@ -0,0 +1,33 @@
+
+# Ques 1
+numpy provides optimized and best methods to solve mathematical operations .
+
+pandas dataframe is a 2d representation of data. say it have a spreadsheets of rows and columns.
+
+
+# Ques 2
+Invalid or non-existent ticker symbol,Ambiguous symbols,Changing symbols,Delisted or suspended stocks.
+
+
+to handle all the above issue you can Use
+Regular updates,Symbol lookup,Error handling,Validate input.
+
+
+# Ques 3
+Bar chart, Line Chart are used for plotting.
+
+
+# Ques 4
+Legend will help comparison between different stocks, so will be essential on a stock market chart
+
+
+# Ques 5
+for limiting the length of a pandas data frame to less than a year we can use start and end parameter.
+
+
+# Ques 6
+it is statiscal analysis used in finalcial analysis  and forecasting.To calculate a 180-day moving average, you would take the sum of the variable's values over the past 180 days and divide it by 180. traders used this feature very freqently to analyze and predict where will market go.
+
+
+# Ques 7
+pov: iam not able to understand the question. 
