773f7aec09ddf668f5da815a072650da6680bc51
Agamdeep Singh <93773431+agamdeepsing@users.noreply.github.com>
2023-05-18 21:58:18 +0530

    Add files via upload

---
+++ PYTHON ADVANCED 23.py
@@ -0,0 +1,45 @@
+
+# Ques 1
+matplotlib provides us the convenient method called subplots for increasing 
+the comparison between different figures on the same graph.
+
+
+# Ques 2
+Compound interest makes a sum of money grow at a faster rate than simple interest,
+because in addition to earning returns on the money you invest, you also earn returns 
+on those returns at the end of every compounding period(monthly, 
+quarterly or annually.)
+
+
+# Ques 3
+hist shows total values of x with respect to y.numpy.histogram() is the built in function used.
+
+
+# Ques 4
+We can use figure(figsize=(10,8)) function inside the matplot.pyplot library which we scale down or 
+up the graph.
+
+
+# Ques 5
+in regular multiplication:
+values of same index get multiplied.
+
+In dot product:
+there is row wise multiplication, row of one array with column of second array and so on.
+
+In outer multiplication:
+every element of first array a1 will be multiply by every element of other array a2
+
+
+# Ques 6
+np.pmt(rate, nper, pv) function we will be using in order to calculate monthly mortgage 
+payment before you purchase a house.
+
+rate--interest rate
+Nper-payment duration
+pv- total Value
+
+
+# Ques 7
+Yes, an array can store the string. limitation is that after storing the string it stores 
+the lenght of the string in the memory and sets that length as the limit.
